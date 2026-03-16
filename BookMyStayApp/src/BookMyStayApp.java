b06cb16b2a3e934ed461ea6cb0e8106d842e0bf1
nexline-01xA <hn9608@srmist.edu.in>
2026-03-16 15:59:13 +0530

    Initial commit - BookMyStayApp

---
+++ BookMyStayApp/src/BookMyStayApp.java
@@ -0,0 +1,18 @@
+public class BookMyStayApp {
+
+    public static void main(String[] args) {
+
+        // Welcome Message
+        System.out.println("==================================");
+        System.out.println("   Welcome to Book My Stay App   ");
+        System.out.println("==================================");
+
+        // Application Details
+        System.out.println("Application Name : Book My Stay App");
+        System.out.println("Version          : 1.0");
+
+        System.out.println("----------------------------------");
+        System.out.println("Application Started Successfully.");
+        System.out.println("----------------------------------");
+    }
+}
